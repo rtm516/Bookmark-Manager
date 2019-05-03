d4c4ca9cf6d51ed58e06af660612093d586cd42a
Ryan Milner <ryantmilner@hotmail.co.uk>
2019-05-03 02:13:57 +0100

    Added bookmark adding and deleting to popup

--- popup.js
+++ popup.js
@@ -1,10 +1,25 @@
+let bookmarkID = "";
+
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	let tab = tabs[0];
 
-	document.querySelector(".colc-tilea>input[type='text']").value = tab.title;
-	document.querySelector(".colc-tilea>input[type='url']").value = tab.url;
+	chrome.bookmarks.search({url: tab.url}, function(results) {
+		if (results.length >= 1) {
+			document.querySelector(".colc-tilea>input[type='text']").value = results[0].title;
+			document.querySelector(".colc-tilea>input[type='url']").value = results[0].url;
 
-	document.querySelector(".colc-img").style = "background-image: url('images/tile-bg-" + (Math.round(Math.random() * 7) + 1) + ".jpg');";
+			bookmarkID = results[0].id;
+		}else{
+			document.querySelector(".colc-tilea>input[type='text']").value = tab.title;
+			document.querySelector(".colc-tilea>input[type='url']").value = tab.url;
+
+			document.querySelector(".colc-img").style = "background-image: url('images/tile-bg-" + (Math.round(Math.random() * 7) + 1) + ".jpg');";
+
+			chrome.bookmarks.create({'title': tab.title, 'url': tab.url}, function(result) {
+				bookmarkID = result.id;
+			});
+		}
+	});
 });
 
 document.querySelector(".colc-viewt").onclick = function() {
@@ -12,5 +27,10 @@ document.querySelector(".colc-viewt").onclick = function() {
 };
 
 document.querySelector(".colc-confi").onclick = function() {
+	if (bookmarkID != "") {
+		chrome.bookmarks.remove(bookmarkID);
+	}
+
 	window.close();
 };
+
